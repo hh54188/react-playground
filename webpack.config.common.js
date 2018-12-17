b672dc64f75d5590fb952d77e1b2f1cba868c086
liguangyi <252438823@qq.com>
2018-12-17 21:17:53 +0800

    update

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -5,8 +5,8 @@ const PUBLIC_DIR_PATH = path.join(__dirname, "public");
 
 module.exports = {
   entry: {
-    app: "./src/index_stopwatch_redux_container.js"
-    // app: "./src/index_stopwatch_redux.js"
+    // app: "./src/index_stopwatch_redux_container.js"
+    app: "./src/index_stopwatch_redux.js"
     // app: "./src/index_stopwatch_mobx.js"
     // app: "./src/index_list_id.js"
     // app: "./src/index_mobx_performance.js"
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,18 +1,18 @@
-const path = require('path')
-const webpack = require('webpack')
-const merge = require('webpack-merge')
-const common = require('./webpack.config.common.js')
+const path = require("path");
+const webpack = require("webpack");
+const merge = require("webpack-merge");
+const common = require("./webpack.config.common.js");
 
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const PUBLIC_DIR_PATH = path.join(__dirname, 'public')
+const PUBLIC_DIR_PATH = path.join(__dirname, "public");
 
 module.exports = merge(common, {
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   devServer: {
     contentBase: PUBLIC_DIR_PATH,
     historyApiFallback: true,
-    open: true,
+    // open: true,
     // openPage: 'exps/android_bottom_bar_exp',
     // openPage: 'exps/create',
     hot: true,
@@ -20,18 +20,18 @@ module.exports = merge(common, {
     //   '/api': 'http://localhost:3000',
     // },
     overlay: {
-      error: true,
-    },
+      error: true
+    }
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'public/index-template.html'),
+      template: path.join(__dirname, "public/index-template.html")
     }),
     new webpack.NamedModulesPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development'),
-    }),
+      "process.env.NODE_ENV": JSON.stringify("development")
+    })
   ],
-  watch: true,
-})
+  watch: true
+});
